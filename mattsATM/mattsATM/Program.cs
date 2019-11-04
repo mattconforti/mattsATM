e723f4f55b7e9194408cdbcd858ad9abf2c5e4c9
Matt Conforti <mattconforti@matts-Mac-mini.local>
2019-11-04 14:40:25 -0500

    created 2 classes, Atm and User (along with main program)

---
+++ mattsATM/mattsATM/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mattsATM
+{
+
+    class Atm
+    {
+
+        public bool debug = true;
+
+        private string bankName;
+
+        private string location;
+
+        // Generic Dictionary Collection
+        private Dictionary<int, string> usrInfoDict;
+
+        public Atm(string inBankName, string inLocation)
+        {
+            this.bankName = inBankName;
+            this.location = inLocation;
+            this.usrInfoDict = new Dictionary<int, string>();
+        }
+
+        public void printInfo()
+        {
+            Console.WriteLine("ATM INFO -------");
+            Console.WriteLine($"Bank: {this.bankName}");
+            Console.WriteLine($"Location: {this.location}");
+        }
+
+        public void userLogIn(User newUser)
+        {
+            string userID = newUser.getID();
+        }
+
+    }
+
+    class User
+    {
+        private string ID;
+
+        private int pin;
+
+        public User(string inID, int inPin)
+        {
+            this.ID = inID;
+            this.pin = inPin;
+        }
+
+        public void setID(string value)
+        {
+            this.ID = value;
+        }
+
+        public string getID()
+        {
+            return this.ID;
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Atm newAtm = new Atm("CitiBank", "New York");
+            User mattC = new User("21", 1234);
+            newAtm.userLogIn(mattC);
+
+            if (newAtm.debug)
+            {
+                newAtm.printInfo();
+            }
+        }
+    }
+}
